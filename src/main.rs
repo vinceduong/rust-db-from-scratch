4bfa1f242bf2a54101e625e96121fb99e63aa5ee
Vincent Duong <vincent.duong.dev@gmail.com>
2024-02-03 10:05:24 +0100

    feat: change from string to binary save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use serde::{Deserialize, Serialize};
 mod storage;
 
-fn main() {}
+#[derive(Serialize, Deserialize, Debug)]
+struct MyStruct {
+    field1: u32,
+    field2: String,
+}
+
+fn main() {
+    let lol = MyStruct {
+        field1: 1,
+        field2: "fdsf".to_string(),
+    };
+
+    storage::write_to_collection::write_to_collection(
+        "./data",
+        bincode::serialize(&lol).unwrap(),
+        "user",
+    );
+}
