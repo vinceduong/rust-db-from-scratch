af758d8519643bdf4e55adf9564bbe0dd2b8df02
Vincent Duong <vincent.duong.dev@gmail.com>
2024-02-03 13:55:44 +0100

    feat: change from string to binary save (#3)

--- src/storage/write_to_collection.rs
+++ src/storage/write_to_collection.rs
@@ -2,7 +2,7 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
 
-pub fn write_to_collection(dir: &str, document: &str, collection: &str) {
+pub fn write_to_collection(dir: &str, document: Vec<u8>, collection: &str) {
     let binding = format!("{}/{}.collection", dir, collection);
     let path = Path::new(&binding);
     let display = path.display();
@@ -13,12 +13,7 @@ pub fn write_to_collection(dir: &str, document: &str, collection: &str) {
         .open(&path)
         .unwrap_or_else(|why| panic!("Couldn't open {}: {}", display, why));
 
-    let document_line = format!("{}\n", document);
-
-    match file.write_all(document_line.as_bytes()) {
-        Err(why) => panic!("couldn't write to {}: {}", display, why),
-        Ok(_) => println!("successfully wrote to {}", display),
-    }
+    file.write_all(&document).expect("Failed to write data")
 }
 
 #[cfg(test)]
@@ -28,10 +23,11 @@ mod tests {
     use std::io::Read;
     use tempfile::tempdir;
 
-    fn read_file_content(path: &Path) -> String {
+    fn read_file_content(path: &Path) -> Vec<u8> {
         let mut file = File::open(path).unwrap();
-        let mut content = String::new();
-        file.read_to_string(&mut content).unwrap();
+        let mut content = Vec::new();
+        file.read_to_end(&mut content).unwrap();
+        println!("CONTENT: {:?}", content);
         content
     }
 
@@ -39,28 +35,34 @@ mod tests {
     fn test_write_to_new_collection() {
         let dir = tempdir().unwrap();
         let collection_name = "new_collection";
-        let document = "New document content";
+        let document = vec![1, 2, 3, 4, 5];
         let collection_path = dir.path().join(format!("{}.collection", collection_name));
 
         // Change the path in the actual function or use any method to redirect output to the temp directory
         // For the sake of this example, assume the function writes to the temp directory
-        write_to_collection(dir.into_path().to_str().unwrap(), document, collection_name);
+        write_to_collection(
+            dir.into_path().to_str().unwrap(),
+            document.clone(),
+            collection_name,
+        );
 
         assert!(collection_path.exists());
         let content = read_file_content(&collection_path);
-        assert_eq!(content, format!("{}\n", document));
+        assert_eq!(content, document);
     }
 
     #[test]
     fn test_append_to_existing_collection() {
         let dir = tempdir().unwrap();
         let collection_name = "existing_collection";
-        let initial_document = "Initial content";
-        let appended_document = "Appended content";
+        let initial_document = vec![1, 2, 3, 4];
+        let appended_document = vec![1, 2, 3, 4];
+
         let collection_path = dir.path().join(format!("{}.collection", collection_name));
         let binding = dir.into_path();
         let dir_name = binding.to_str().unwrap();
-
+        let merged_vec: Vec<u8> =
+            [initial_document.as_slice(), appended_document.as_slice()].concat();
         // Write initial content
         write_to_collection(dir_name, initial_document, collection_name);
         // Append new content
@@ -68,10 +70,7 @@ mod tests {
 
         assert!(collection_path.exists());
         let content = read_file_content(&collection_path);
-        assert_eq!(
-            content,
-            format!("{}\n{}\n", initial_document, appended_document)
-        );
+        assert_eq!(content, merged_vec);
     }
 
     // Add more tests here for error handling, etc.
