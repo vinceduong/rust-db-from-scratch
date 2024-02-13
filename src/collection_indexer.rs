d8c9361d5e4cc5037d495b6c1a42675db6781b9b
Vincent Duong <vincent.duong.dev@gmail.com>
2024-02-13 21:59:51 +0100

    feat: add tests (#10)

--- src/collection_indexer.rs
+++ src/collection_indexer.rs
@@ -29,7 +29,7 @@ fn index_collection_hash<T: Document>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::document::HasId;
+    use crate::{collection_page::CollectionPage, document::HasId};
     use serde_derive::{Deserialize, Serialize};
     use tempfile::tempdir;
 
@@ -47,7 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn test_collection_hash() {
+    fn test_collection_hash_one_document() {
         let dir = tempdir().unwrap();
         let binding = dir.into_path();
         let dir_name = binding.to_str().unwrap();
@@ -65,6 +65,62 @@ mod tests {
 
         let mut expected_hash_map = HashMap::new();
         expected_hash_map.insert(1, 0);
+
+        assert_eq!(hash, expected_hash_map)
+    }
+
+    #[test]
+    fn test_collection_hash_two_document() {
+        let dir = tempdir().unwrap();
+        let binding = dir.into_path();
+        let dir_name = binding.to_str().unwrap();
+
+        let mut collection_file = CollectionFile::<MyDocument>::new("test", dir_name).unwrap();
+
+        let mut collection_page = collection_file.read_page(0).unwrap();
+
+        collection_page
+            .insert_document(MyDocument { id: 1 })
+            .unwrap();
+        collection_page
+            .insert_document(MyDocument { id: 2 })
+            .unwrap();
+        collection_file.write_page(&collection_page).unwrap();
+
+        let hash = index_collection_hash(collection_file).unwrap();
+
+        let mut expected_hash_map = HashMap::new();
+        expected_hash_map.insert(1, 0);
+        expected_hash_map.insert(2, 0);
+
+        assert_eq!(hash, expected_hash_map)
+    }
+
+    #[test]
+    fn test_collection_hash_two_document_in_two_pages() {
+        let dir = tempdir().unwrap();
+        let binding = dir.into_path();
+        let dir_name = binding.to_str().unwrap();
+
+        let mut collection_file = CollectionFile::<MyDocument>::new("test", dir_name).unwrap();
+
+        let mut collection_page_0 = collection_file.read_page(0).unwrap();
+        let mut collection_page_1 = CollectionPage::<MyDocument>::new(1);
+
+        collection_page_0
+            .insert_document(MyDocument { id: 1 })
+            .unwrap();
+        collection_page_1
+            .insert_document(MyDocument { id: 2 })
+            .unwrap();
+        collection_file.write_page(&collection_page_0).unwrap();
+        collection_file.write_page(&collection_page_1).unwrap();
+
+        let hash = index_collection_hash(collection_file).unwrap();
+
+        let mut expected_hash_map = HashMap::new();
+        expected_hash_map.insert(1, 0);
+        expected_hash_map.insert(2, 1);
 
         assert_eq!(hash, expected_hash_map)
     }
